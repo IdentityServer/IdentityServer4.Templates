a6534b58f8c80f39ebf54cb415e7b7c5ea3ae5a7
Dominick Baier <dbaier@leastprivilege.com>
2017-04-05 08:39:02 +0200

    initial commit

---
+++ src/IdentityServer4Basic/Config.cs
@@ -0,0 +1,33 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer4Basic
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new[]
+            {
+                new ApiResource("api1", "My API #1")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    ClientName = "Client credentials grant",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("373f4671-0c18-48d6-9da3-962b1c81299a".Sha256()) },
+
+                    AllowedScopes = { "api1" }
+                }
+            };
+        }
+    }
+}
---
+++ src/IdentityServer4Basic/Startup.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace IdentityServer4Basic
+{
+    public class Startup
+    {
+        public IHostingEnvironment Environment { get; }
+
+        public Startup(ILoggerFactory loggerFactory, IHostingEnvironment environment)
+        {
+            Environment = environment;
+
+            var serilog = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .Enrich.FromLogContext()
+                .WriteTo.File(@"identityserver4_log.txt");
+
+            if (Environment.IsDevelopment())
+            {
+                serilog.WriteTo.LiterateConsole(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message}{NewLine}{Exception}{NewLine}");
+            }
+
+            loggerFactory
+                .WithFilter(new FilterLoggerSettings
+                {
+                    { "IdentityServer4", LogLevel.Debug },
+                    { "Microsoft", LogLevel.Information },
+                    { "System", LogLevel.Error },
+                })
+                .AddSerilog(serilog.CreateLogger());
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(Config.GetApis())
+                .AddInMemoryClients(Config.GetClients());
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+        }
+    }
+}
