0cbab7be93466518f6d487e7184a00613aca362a
Brock Allen <brockallen@gmail.com>
2018-01-09 14:25:42 -0500

    update migration for template

--- src/IdentityServer4AspNetIdentity/Data/Migrations/20180109192453_CreateIdentitySchema.Designer.cs
+++ src/IdentityServer4AspNetIdentity/Data/Migrations/20180109192453_CreateIdentitySchema.Designer.cs
@@ -11,14 +11,14 @@ using System;
 namespace IdentityServer4AspNetIdentity.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20171119171739_CreateIdentitySchema")]
+    [Migration("20180109192453_CreateIdentitySchema")]
     partial class CreateIdentitySchema
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.0.0-rtm-26452");
+                .HasAnnotation("ProductVersion", "2.0.1-rtm-125");
 
             modelBuilder.Entity("IdentityServer4AspNetIdentity.Models.ApplicationUser", b =>
                 {
