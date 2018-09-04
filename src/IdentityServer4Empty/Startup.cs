6ed397d5617fce196ee6110fdfa81ff894433c3a
Dominick Baier <dbaier@leastprivilege.com>
2018-09-04 08:40:35 +0200

    add MVC bits to empty (commented out)

--- src/IdentityServer4Empty/Startup.cs
+++ src/IdentityServer4Empty/Startup.cs
@@ -20,6 +20,9 @@ namespace IdentityServer4Empty
 
         public void ConfigureServices(IServiceCollection services)
         {
+            // uncomment, if you wan to add an MVC-based UI
+            //services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
+
             var builder = services.AddIdentityServer()
                 .AddInMemoryIdentityResources(Config.GetIdentityResources())
                 .AddInMemoryApiResources(Config.GetApis())
@@ -42,7 +45,13 @@ namespace IdentityServer4Empty
                 app.UseDeveloperExceptionPage();
             }
 
+            // uncomment if you want to support static files
+            //app.UseStaticFiles();
+
             app.UseIdentityServer();
+
+            // uncomment, if you wan to add an MVC-based UI
+            //app.UseMvcWithDefaultRoute();
         }
     }
 }
