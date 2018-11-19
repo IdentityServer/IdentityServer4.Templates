e4a5b0b05ec3ee2189ee751e4a02aecc677fb7a4
Scott Brady <scott@scottbrady91.com>
2018-11-19 15:23:07 +0000

    Updated QuickstartUI

--- src/IdentityServer4Admin/Quickstart/Device/DeviceAuthorizationInputModel.cs
+++ src/IdentityServer4Admin/Quickstart/Device/DeviceAuthorizationInputModel.cs
@@ -2,10 +2,10 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
-namespace IdentityServer4.Quickstart.UI
+namespace IdentityServer4.Quickstart.UI.Device
 {
-    public class LogoutViewModel : LogoutInputModel
+    public class DeviceAuthorizationInputModel : ConsentInputModel
     {
-        public bool ShowLogoutPrompt { get; set; } = true;
+        public string UserCode { get; set; }
     }
 }
