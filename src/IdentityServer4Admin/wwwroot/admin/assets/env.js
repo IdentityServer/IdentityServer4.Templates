9dd5e108059491b99b254e69393a712d33d1e754
Sam Jones <sam.uk.jones@gmail.com>
2019-07-12 10:39:24 +0100

    Re-Added AddUserPassword

--- src/IdentityServer4Admin/wwwroot/admin/assets/env.js
+++ src/IdentityServer4Admin/wwwroot/admin/assets/env.js
@@ -1,5 +1,6 @@
 var ENV = {
     ApiUrl: 'http://localhost:5000',
     UiUrl: 'http://localhost:5000',
-    AuthorityUrl: 'http://localhost:5000'   
+    AuthorityUrl: 'http://localhost:5000',
+	AddUserPassword: true
 };
