36d80d09eaf721bb517acf09c76813f36fb59d0d
David Nguyen <vi.nguyen@mum.edu>
2019-09-29 16:14:08 -0500

    update to sdk 3.0
    I'm gonna fix the build problem and make it work with new sdk 3.0

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 $CakeVersion = "0.30.0"
-$DotNetVersion = "2.1.504";
+$DotNetVersion = "3.0";
 $DotNetInstallerUri = "https://dot.net/v1/dotnet-install.ps1";
 
 $TOOLS_DIR = Join-Path $PSScriptRoot "tools"
