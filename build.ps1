b2b1c0bf266734665409dba50089d0cd5bbf5c1e
Dominick Baier <dbaier@leastprivilege.com>
2019-10-14 08:51:35 +0200

    update version

--- build.ps1
+++ build.ps1
@@ -1,6 +1,4 @@
 $CakeVersion = "0.30.0"
-$DotNetVersion = "2.1.504";
-$DotNetInstallerUri = "https://dot.net/v1/dotnet-install.ps1";
 
 $TOOLS_DIR = Join-Path $PSScriptRoot "tools"
 $NUGET_EXE = Join-Path $TOOLS_DIR "nuget.exe"
