251208e6d75fcf99ee9a20516c08d3472c8d4152
CMyhill <Christopher.Myhill@Rocksolidknowlege.com>
2020-04-15 10:45:23 +0100

    Update template to use netcore 3.1 and adminui 3.0

--- src/IdentityServer4Admin/wwwroot/admin/assets/env.js
+++ src/IdentityServer4Admin/wwwroot/admin/assets/env.js
@@ -1,7 +1,7 @@
 var ENV = {
     ApiUrl: 'http://localhost:5000',
     UiUrl: 'http://localhost:5000',
-    AuthorityUrl: 'http://localhost:5000',
+    AuthorityUrl: 'http://localhost:5000',   
     AddUserPassword: true,
     CommunityEdition: true,
     UserManagementDisabled: false
